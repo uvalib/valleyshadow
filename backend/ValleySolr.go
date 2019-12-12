f4a20c8fd42325a87ecf5605f0691bb2e65f7335
Tim Stevens <tss6n@virginia.edu>
2019-12-12 13:29:44 -0500

    removing some debugging lines

--- backend/ValleySolr.go
+++ backend/ValleySolr.go
@@ -6,19 +6,18 @@
 
 package main
 
-
 /* ===================================================================================
 
 		Declare imports
 
 ====================================================================================*/
 
-
 import (
 	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
+	"os"
 )
 
 /* ===================================================================================
@@ -38,17 +37,17 @@ var (
 /* ===================================================================================
 
 		Custom Solr response struct
-		
+
 ====================================================================================*/
 
 type SolrQuery struct {
-	Rows     int
-	Start    int
-	Query    string
-	Facet    string // not used
-	Filter   string // not used
-	Sort     string 
-	Params   map[string]interface{}
+	Rows   int
+	Start  int
+	Query  string
+	Facet  string // not used
+	Filter string // not used
+	Sort   string
+	Params map[string]interface{}
 }
 
 /* ---------------------------------------------------------------------------------*/
@@ -57,10 +56,10 @@ type DocumentCollection struct {
 	Docs     []map[string]interface{}
 	NumFound int
 	Start    int
-	ST		 string	
-	CURSOR	 int
-	QS		 string
-	County	 string
+	ST       string
+	CURSOR   int
+	QS       string
+	County   string
 	Year     string
 	//Backdoor *datapipe
 }
@@ -73,57 +72,53 @@ type SolrResponse struct {
 	Status   int
 	QTime    int
 	Error    error
-	
 }
 
 /* ===================================================================================
 
 		Funcs to retrieve site or solr host url's
-		
+
 ====================================================================================*/
 
-
-func GetSolrUrl() string	{
-	return "http://libsvr40.lib.virginia.edu:8080/solr/valley/select?"
+func GetSolrUrl() string {
+	return os.Getenv("solr_url")
 }
 
 /* ---------------------------------------------------------------------------------*/
 
-
-func GetSiteUrl() string	{
-	return "http://localhost:8080"
+func GetSiteUrl() string {
+	return os.Getenv("site_url")
 }
 
 /* ===================================================================================
 
 		Builds a solr query
-		
+
 ====================================================================================*/
 
+func ConstructSolrQuery(x string) string {
 
-func  ConstructSolrQuery(x string) string {
-		
-		qun, err := url.QueryUnescape(x)
-		if err != nil {
+	qun, err := url.QueryUnescape(x)
+	if err != nil {
 		panic(err)
 	}
-		m, _ := url.ParseQuery(qun)
-		
-		params := url.Values{}
-		params.Add("q",  m.Get("q"))
-		params.Add("rows", "50")
-		params.Add("start", m.Get("start"))
-		params.Add("sort", m.Get("sort"))
-		params.Add("facet", "")
-		params.Add("filter", "")
-		params.Add("hl", "true")
-		params.Add("hl.fl", "fulltext")
-		params.Add("hl.fragsize", "200")
-		params.Add("hl", "*,score")
-		params.Add("wt", "json")
+	m, _ := url.ParseQuery(qun)
 
-		finalUrl := GetSolrUrl() + params.Encode()
-	
+	params := url.Values{}
+	params.Add("q", m.Get("q"))
+	params.Add("rows", "50")
+	params.Add("start", m.Get("start"))
+	params.Add("sort", m.Get("sort"))
+	params.Add("facet", "")
+	params.Add("filter", "")
+	params.Add("hl", "true")
+	params.Add("hl.fl", "fulltext")
+	params.Add("hl.fragsize", "200")
+	params.Add("hl", "*,score")
+	params.Add("wt", "json")
+
+	finalUrl := GetSolrUrl() + params.Encode()
+
 	return finalUrl
 }
 
@@ -135,28 +130,23 @@ func  ConstructSolrQuery(x string) string {
 
 ====================================================================================*/
 
-
 func PopulateResponse(j map[string]interface{}) (*SolrResponse, error) {
 
 	response_root := j
 	response := response_root["response"]
 
-
 	r := SolrResponse{}
-
 
 	r_header, ok := response_root["responseHeader"].(map[string]interface{})
 	if !ok {
 		return nil, ErrNoResponseHeader
 	}
-	
 
 	if status, ok := r_header["status"]; ok {
 		r.Status = int(status.(float64))
 	} else {
 		return nil, ErrInvalidHeader
 	}
-
 
 	if qtime, ok := r_header["QTime"]; ok {
 		r.QTime = int(qtime.(float64))
@@ -184,16 +174,12 @@ func PopulateResponse(j map[string]interface{}) (*SolrResponse, error) {
 		num_found := int(responseMap["numFound"].(float64))
 		fmt.Printf("NumFound: %v \n", num_found)
 
-
 		// and the amount actually returned
 		num_results := len(docsSlice)
 		fmt.Printf("NumReturned: %v \n", num_results)
 
-
 		coll := DocumentCollection{}
 		coll.NumFound = num_found
-		
-
 
 		ds := make([]map[string]interface{}, 0, num_results)
 
@@ -232,20 +218,18 @@ func PopulateResponse(j map[string]interface{}) (*SolrResponse, error) {
 }
 
 /* ===================================================================================
-	
-		Solr json response is unmarshalled into a custom struct 
-		
+
+		Solr json response is unmarshalled into a custom struct
+
 ====================================================================================*/
 
-	
 func SolrFromHTTP(b []byte) (*SolrResponse, error) {
-	
-		// SAVE TO TROUBLESHOOT
-		// fmt.Printf("container : %v", string(b))	
+
+	// SAVE TO TROUBLESHOOT
+	// fmt.Printf("container : %v", string(b))
 
 	var container map[string]interface{}
-	
-	
+
 	err := json.Unmarshal(b, &container)
 	if err != nil {
 		return nil, err
